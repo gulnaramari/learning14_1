922657f1a02079e5f353eb2da7b68526a15d83d0
Daniel <gulnarikmihailova@gmail.com>
2025-02-05 23:50:14 +0300

    1 задание

--- src/category.py
+++ src/category.py
@@ -21,6 +21,13 @@ class Category:
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
 
+    def __str__(self):
+        products_quantity = 0
+        for product in self.__products:
+            products_quantity += product.quantity
+        return f'{self.name}, количество продуктов: {products_quantity} шт.'
+
+
     def add_product(self, product: Product) -> Any:
         if isinstance(product, Product):
             self.__products.append(product)
@@ -47,6 +54,10 @@ if __name__ == "__main__":  # pragma: no cover
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
+    print(str(product1))
+    print(str(product2))
+    print(str(product3))
+
     print(product1.name)
     print(product1.description)
     print(product1.price)
@@ -63,13 +74,11 @@ if __name__ == "__main__":  # pragma: no cover
     print(product3.quantity)
     category1 = Category(
         "Смартфоны",
-        (
-            "Смартфоны, как средство не только коммуникации,"
-            "но и получения дополнительных функций"
-            " для удобства жизни"
-        ),
-        [product1, product2, product3],
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3]
     )
+
+    print(str(category1))
 
     print(category1.name == "Смартфоны")
     print(category1.description)
@@ -91,3 +100,23 @@ if __name__ == "__main__":  # pragma: no cover
     print(category1.products_in_list)
     products_in_list = product1, product2, product3, product4
     print(Product.new_product(products_in_list))
+
+
+if __name__ == '__main__':
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    print(str(product1))
+    print(str(product2))
+    print(str(product3))
+
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3]
+    )
+
+    print(str(category1))
+
+    print(category1.products)
